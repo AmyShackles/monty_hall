ba0337fb0b97daa3829830f0d7920273afe71885
Amy Shackles <amyshackles@gmail.com>
2020-06-15 17:38:39 -0700

    Adds score tracking

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import "./App.css";
 import { Gameshow } from "./components/Gameshow.js";
 
 function App() {
+  const [score, setScore] = React.useState(0);
+  const [gamesPlayed, setGamesPlayed] = React.useState(0);
+  const [switched, setSwitched] = React.useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Monty Hall Problem</h1>
       </header>
-      <Gameshow />
+      <div className="score">
+        <p>
+          {score} wins/{gamesPlayed} games
+        </p>
+        <p>
+          You switched doors {switched} times (
+          {gamesPlayed === 0
+            ? "0.00"
+            : ((switched / gamesPlayed) * 100).toFixed(2)}
+          %)
+        </p>
+        <p>
+          You opted to not switch doors {gamesPlayed - switched} times (
+          {gamesPlayed === 0
+            ? "0.00"
+            : (((gamesPlayed - switched) / gamesPlayed) * 100).toFixed(2)}
+          %)
+        </p>
+        <p>
+          Your win percentage is{" "}
+          {gamesPlayed === 0
+            ? "0.00"
+            : ((score / gamesPlayed) * 100).toFixed(2)}
+          %
+        </p>
+      </div>
+      <h2>Select a door</h2>
+      <Gameshow
+        setScore={setScore}
+        setGamesPlayed={setGamesPlayed}
+        setSwitched={setSwitched}
+      />
     </div>
   );
 }
--- src/components/Gameshow.js
+++ src/components/Gameshow.js
@@ -3,14 +3,21 @@ import { Prompt } from "./Prompt.js";
 import { Door } from "./Door.js";
 import { MontyHall } from "./MontyHall.js";
 
-const Gameshow = () => {
+const Gameshow = ({ setScore, setGamesPlayed, setSwitched }) => {
   const [promptOpen, setPromptOpen] = React.useState(false);
   const [result, setResult] = React.useState("");
-  const [game, setGame] = React.useState(null);
-
+  const [game, setGame] = React.useState({});
+  React.useEffect(() => {
+    if (game.win === true) {
+      setScore((score) => score + 1);
+    }
+    if (game.win === true || game.win === false) {
+      setGamesPlayed((gamesPlayed) => gamesPlayed + 1);
+    }
+  }, [game.win, setScore, setGamesPlayed]);
   const handleSelection = (event) => {
     const door = event.target.name;
-    if (!game) {
+    if (!game.contestantDoor) {
       setGame(new MontyHall(door));
       setPromptOpen(true);
     }
@@ -21,11 +28,12 @@ const Gameshow = () => {
   };
   const changeDoor = () => {
     setResult(game.changeAnswer());
+    setSwitched((switched) => switched + 1);
     setPromptOpen(false);
   };
   const restart = () => {
     setResult("");
-    setGame(null);
+    setGame({});
   };
   return (
     <main>
