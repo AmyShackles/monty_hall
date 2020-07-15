c92befc2cbd9529aeb7ae058260fee39c5783a30
Amy Shackles <amyshackles@gmail.com>
2020-07-14 17:02:49 -0700

    Adds a description and styles results

--- src/App.js
+++ src/App.js
@@ -6,43 +6,66 @@ function App() {
   const [score, setScore] = React.useState(0);
   const [gamesPlayed, setGamesPlayed] = React.useState(0);
   const [switched, setSwitched] = React.useState(0);
+  const [modalOpen, setModalOpen] = React.useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Monty Hall Problem</h1>
       </header>
-      {score ? <div className="score">
-        <p>
-          {score} wins/{gamesPlayed} games
-        </p>
-        <p>
-          You switched doors {switched} times (
-          {gamesPlayed === 0
-            ? "0.00"
-            : ((switched / gamesPlayed) * 100).toFixed(2)}
-          %)
-        </p>
-        <p>
-          You opted to not switch doors {gamesPlayed - switched} times (
-          {gamesPlayed === 0
-            ? "0.00"
-            : (((gamesPlayed - switched) / gamesPlayed) * 100).toFixed(2)}
-          %)
-        </p>
-        <p>
-          Your win percentage is{" "}
-          {gamesPlayed === 0
-            ? "0.00"
-            : ((score / gamesPlayed) * 100).toFixed(2)}
-          %
-        </p>
-      </div> : null}
-      {!switched && <h2>Select a door</h2>}
+
+{!score && !gamesPlayed ? <div className="description">
+  <p>The Monty Hall Problem is a brain teaser based around the American TV game show <i>Let's Make a Deal</i> and named after that show's original host.  The general premise of the problem is as follows:</p>
+  <p>You are presented with three doors.  Behind two of the doors, there is a goat.  Behind the third is a prize.  You are asked to make a guess at which of the three doors contains the prize.  Monty Hall then opens one of the doors to reveal a goat.  You are given the option to keep your original door or switch doors.</p>
+  <p>Which one is more likely to result in you winning?  Are the chances equal?</p>
+</div> : null }
+      {!modalOpen && <h2>Select a door</h2>}
       <Gameshow
         setScore={setScore}
         setGamesPlayed={setGamesPlayed}
         setSwitched={setSwitched}
+        setModalOpen={setModalOpen}
       />
+      <div className="table">
+      {score || gamesPlayed ? <table className="score">
+        <caption>Results</caption>
+        <thead>
+          <tr>
+            <th>In {gamesPlayed} {gamesPlayed === 1 ? 'game' : 'games'}</th>
+            <th>#</th>
+            <th>%</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Wins</th>
+              <td>{score}</td>
+              <td>{gamesPlayed === 0
+            ? "0.00"
+            : ((score / gamesPlayed) * 100).toFixed(2)}
+          %</td>
+            </tr>
+            <tr>
+              <th>Switched</th>
+              <td>{switched}</td>
+              <td> {gamesPlayed === 0
+            ? "0.00"
+            : ((switched / gamesPlayed) * 100).toFixed(2)}
+          %</td>
+            </tr>
+            <tr>
+              <th>Stayed</th>
+              <td>{gamesPlayed - switched}</td>
+              <td> {gamesPlayed === 0
+            ? "0.00"
+            : (((gamesPlayed - switched) / gamesPlayed) * 100).toFixed(2)}
+          %</td>
+            </tr>
+          </tbody>
+          </table>
+
+: ''}
+    </div>
     </div>
   );
 }
--- src/components/Gameshow.js
+++ src/components/Gameshow.js
@@ -3,7 +3,7 @@ import { Prompt } from "./Prompt.js";
 import { Door } from "./Door.js";
 import { MontyHall } from "./MontyHall.js";
 
-const Gameshow = ({ setScore, setGamesPlayed, setSwitched }) => {
+const Gameshow = ({ setScore, setGamesPlayed, setSwitched, setModalOpen }) => {
   const [promptOpen, setPromptOpen] = React.useState(false);
   const [result, setResult] = React.useState("");
   const [game, setGame] = React.useState({});
@@ -16,7 +16,9 @@ const Gameshow = ({ setScore, setGamesPlayed, setSwitched }) => {
       setGamesPlayed((gamesPlayed) => gamesPlayed + 1);
     }
   }, [game.win, setScore, setGamesPlayed]);
+
   const handleSelection = (event) => {
+    setModalOpen(true);
     const door = event.target.name;
     if (!game.contestantDoor) {
       setGame(new MontyHall(door));
@@ -37,6 +39,7 @@ const Gameshow = ({ setScore, setGamesPlayed, setSwitched }) => {
   const restart = () => {
     setResult("");
     setGame({});
+    setModalOpen(false);
   };
   return (
     <main>
@@ -52,6 +55,7 @@ const Gameshow = ({ setScore, setGamesPlayed, setSwitched }) => {
         <Door id="2" isOpen={promptOpen} game={game || {}} handleSelection={handleSelection} />
         <Door id="3" isOpen={promptOpen} game={game || {}} handleSelection={handleSelection} />
       </div>
+      <br/>
       {selected ? <button onClick={() => setPromptOpen(true)}>Decide Next Move</button> : null}
       {result && (
         <>
