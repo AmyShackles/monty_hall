22ae033551cb02da377a97f976849cc89023e288
Amy Shackles <amyshackles@gmail.com>
2020-08-31 13:33:57 -0700

    Removed unused dependencies, changed manifest rel to preload

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders Monty Hall Problem", () => {
-  const { getByText } = render(<App />);
-  const title = getByText(/Monty Hall Problem/i);
-  expect(title).toBeInTheDocument();
-});
